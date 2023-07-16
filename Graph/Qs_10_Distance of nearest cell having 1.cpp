22e43c1220665e9745fac1e7083ff34e8b3146af
Rohan <139442292+Roh727an@users.noreply.github.com>
2023-07-16 12:14:21 +0530

    Create Qs_10_Distance of nearest cell having 1.cpp
    Given a binary grid of n*m. Find the distance of the nearest 1 in the grid for each cell.
    The distance is calculated as |i1  - i2| + |j1 - j2|, where i1, j1 are the row number and column number of the current cell, and i2, j2 are the row number and column number of the nearest cell having value 1. There should be atleast one 1 in the grid.

---
+++ Graph/Qs_10_Distance of nearest cell having 1.cpp
@@ -0,0 +1,52 @@
+class Solution 
+{
+    public:
+    //Function to find distance of nearest 1 in the grid for each cell.
+	vector<vector<int>>nearest(vector<vector<int>>grid)
+	{
+	    // Code here
+	    int n=grid.size();
+	    int m=grid[0].size();
+	   // Create a Visitd Matrx
+	   vector<vector<int>>vis(n,vector<int>(m,0));
+	   // Create a distance Matrx
+	   vector<vector<int>>dis(n,vector<int>(m,0));
+	   //Create a Queue {{row,col},dis}
+	   queue<pair<pair<int,int>,int>>q;
+	   for(int i=0;i<n;i++)
+	   {
+	       for(int j=0;j<m;j++) 
+	       {
+	           if(grid[i][j]==1)
+	           {
+	               q.push({{i,j},0});
+	               vis[i][j]=1;
+	           }
+	           else
+	           vis[i][j]=0;
+	       }
+	   }
+	    int delrow[] = {-1, 0, +1, 0}; 
+	    int delcol[] = {0, +1, 0, -1}; 
+	   while(!q.empty())
+	   {
+	       
+	       int row=q.front().first.first;
+	       int col=q.front().first.second;
+	       int step=q.front().second;
+	       q.pop();
+	       dis[row][col]=step;
+	       for(int i=0;i<4;i++)
+	       {
+	           int nrow=row+delrow[i];
+	           int ncol=col+delcol[i];
+	           if(nrow<n && nrow>=0 && ncol>=0 && ncol<m && vis[nrow][ncol]==0)
+	           {
+	               vis[nrow][ncol]=1;
+	               q.push({{nrow,ncol},step+1});
+	           }
+	       }
+	   }
+	   return dis;
+	}
+};
