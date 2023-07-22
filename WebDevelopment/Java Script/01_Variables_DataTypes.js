df92a4078a228ebd4dc6a2db96d1e11ac01dfc3c
Rohan <139442292+Roh727an@users.noreply.github.com>
2023-07-22 11:15:00 +0530

    Create 01_Variables_DataTypes.js
    About Variables & Data Types in Java Script(Lecture 1)

---
+++ WebDevelopment/Java Script/01_Variables_DataTypes.js
@@ -0,0 +1,41 @@
+// a is String
+console.log('For String Data Type');
+let a="xyz";
+console.log(typeof(a));
+a=Number(a);
+console.log('Nmber(a)=',a);
+a=String(a);
+console.log('String(a)=',a);
+a=Boolean(a);
+console.log('Boolean(a)=',a);
+// a is number
+console.log('For Number Data Type');
+a=undefined;
+console.log(typeof(a));
+a=Number(a);
+console.log('Nmber(a)=',a);
+a=String(a);
+console.log('String(a)=',a);
+a=Boolean(a);
+console.log('Boolean(a)=',a);
+// a is undefined
+console.log('For Undefined Data Type');
+a=undefined;
+console.log(typeof(a));
+a=Number(a);
+console.log('Nmber(a)=',a);
+a=String(a);
+console.log('String(a)=',a);
+a=Boolean(a);
+console.log('Boolean(a)=',a);
+// a is Null
+console.log('For Null Data Type');
+a=null;
+console.log(typeof(a));
+a=Number(a);
+console.log('Nmber(a)=',a);
+a=String(a);
+console.log('String(a)=',a);
+a=Boolean(a);
+console.log('Boolean(a)=',a);
+
