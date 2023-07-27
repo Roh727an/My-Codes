0cf0d024d11ac94946367704e981edb50815b393
Rohan <139442292+Roh727an@users.noreply.github.com>
2023-07-27 18:50:18 +0530

    Create 14_Remove duplicates in small prime array.cpp
    Vector STL(find())

---
+++ Placement/Array Easy Questions/14_Remove duplicates in small prime array.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+  public:
+    vector<int> removeDuplicate(vector<int>& arr, int n)
+    {
+        // code here
+        vector<int>ans;
+        for(int i=0;i<n;i++)
+        {
+            auto check=find(ans.begin(),ans.end(),arr[i]);
+            if(check==ans.end())
+            ans.push_back(arr[i]);
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        int N;
+        cin>>N;
+        vector<int> A(N);
+        for(int i=0;i<N;i++)
+        {
+            cin>>A[i];
+        }
+        Solution obj;
+        vector<int>result = obj.removeDuplicate(A,N);
+        for(int i =0;i<result.size();i++)
+            cout<<result[i]<<" ";
+        cout<<endl;
+
+    }
+}
+
+// } Driver Code Ends
