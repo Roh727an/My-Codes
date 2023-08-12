5d5e5ac3640d1eaa171ced647b493eea12908bfa
Rohan Saha <139442292+Roh727an@users.noreply.github.com>
2023-08-12 21:27:25 +0530

    Create Question1.cpp
    # Bits++

---
+++ CodeForces/Question1.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int x = 0;
+    for (int i = 0; i < n; i++)
+    {
+        string s;
+        cin >> s;
+
+        // ++X
+        if (s == "++X")
+        {
+            ++x;
+        }
+        // X++
+        else if (s == "X++")
+        {
+            x++;
+        }
+        // X--
+        else if (s == "X--")
+        {
+            x--;
+        }
+        // --X
+        else if (s == "--X")
+        {
+            --x;
+        }
+    }
+    cout << x << endl;
+    return 0;
+}
